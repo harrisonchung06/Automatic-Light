c3a9576957a4aaf180409a3cf0880aa0e5953bed
Harrison <harrison.chung06@gmail.com>
2025-12-25 10:28:02 -0800

    Changed Position and Speed

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -7,7 +7,7 @@ int neutral_pos;
 
 void cw(int target);
 void ccw(int target);
-void neutral(); 
+void neutral();
 
 void setup() {
   Serial.begin(9600);
@@ -21,13 +21,13 @@ void setup() {
 void loop() {
 
   if (digitalRead(3) == 0 && !flag){
-    cw(neutral_pos+30); 
+    cw(neutral_pos+35); 
     flag = true;
     Serial.print("turn on!"); 
   } 
 
   if (digitalRead(3) == 0 && flag){
-    ccw(neutral_pos-30); 
+    ccw(neutral_pos-60); 
     flag = false;
     Serial.print("turn off!"); 
   } 
@@ -41,7 +41,7 @@ void cw(int target) {
   int pos = s.read();
   for (int i=pos; i<=target; i++){
     s.write(i);
-    delay(100);
+    delay(5);
   }
 }
 
@@ -49,7 +49,7 @@ void ccw(int target) {
   int pos = s.read();
   for (int i=pos; i>=target; i--){
     s.write(i);
-    delay(100);
+    delay(5);
   }
 }
 
@@ -58,13 +58,13 @@ void neutral(){
   if (pos < neutral_pos){
     for (int i=pos; i<=neutral_pos; i++) {
     s.write(i);
-    delay(100); 
+    delay(5); 
     }
   }
   if (pos > neutral_pos){
     for (int i=pos; i>=neutral_pos; i--) {
     s.write(i);
-    delay(100);
+    delay(5);
     }
   } 
 }
